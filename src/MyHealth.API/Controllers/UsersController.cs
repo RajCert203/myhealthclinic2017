6b7cd7e9ac9a156ec8d23e96e03d2d878fca0bec
rajnallella <raj_nallella@outlook.com>
2019-08-28 17:04:51 +0100

    add1

--- src/MyHealth.API/Controllers/UsersController.cs
+++ src/MyHealth.API/Controllers/UsersController.cs
@@ -15,7 +15,7 @@ using MyHealth.API.Validators;
 
 namespace MyHealth.API.Controllers
 {
-    // Comment added 28/08/2019 14:47 15:28 added 1603 	
+    // Comment added 28/08/2019 14:47 15:28 added 1603 1704 	
     public struct ApplicationUserAddRequest
     {
         public ApplicationUser user { get; set; }
